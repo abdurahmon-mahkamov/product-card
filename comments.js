2f448038166e1bcee6578eb7beb1776154b5c277
abdurahmon-mahkamov <abdurakhmon363@gmail.com>
2026-07-29 20:58:28 +0300

    Complete Task #9: Advanced array methods and ES Modules

---
+++ comments.js
@@ -0,0 +1,74 @@
+// comments.js
+// Задание 5: Массив объектов-комментариев (первые 10 объектов)
+export const socialMediaComments = [
+  {
+    postId: 1,
+    id: 1,
+    name: "id labore ex et quam laborum",
+    email: "Eliseo@gardner.biz",
+    body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus dolor quam autem quasi\nrerum odio reprehenderit sit dolor ipsum quibusdam aut"
+  },
+  {
+    postId: 1,
+    id: 2,
+    name: "quo vero reiciendis velit similique earum",
+    email: "Jayne_Kuhic@sydney.com",
+    body: "est natus enim nihil est dolore omnis voluptatem\nnumquam et omnis architecto excepturi optio deserunt\nexpedita eum et molestiae aut adipisci sint dolore pariatur"
+  },
+  {
+    postId: 1,
+    id: 3,
+    name: "odio adipisci rerum aut animi",
+    email: "Nikita@garfield.biz",
+    body: "quia etsuscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt"
+  },
+  {
+    postId: 1,
+    id: 4,
+    name: "alias odio sit",
+    email: "Lew@alysha.tv",
+    body: "non et atque\noccaecati deserunt quas accusantium unde odit nobis\nquia voluptatem quia et\nlibero in deleniti impedit sit sunt"
+  },
+  {
+    postId: 1,
+    id: 5,
+    name: "vero eaque Nam quod",
+    email: "Presley.Mueller@myrl.com",
+    body: "dolores ex illo omnis in et et sint\nquia et et inventore\nratione quia et ut aut\nvel et non iusto\nvoluptas"
+  },
+  {
+    postId: 2,
+    id: 6,
+    name: "et omnis dolorem",
+    email: "Dallas@runo.biz",
+    body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid ab\nut fuga et molestiae est et"
+  },
+  {
+    postId: 2,
+    id: 7,
+    name: "repellat fugiat in",
+    email: "Mallory_Hyatt@davion.biz",
+    body: "repellat aliquid et rerum\nnumquam et omnis architecto excepturi optio deserunt\nexpedita eum et molestiae aut adipisci sint dolore pariatur"
+  },
+  {
+    postId: 2,
+    id: 8,
+    name: "et omnis dolorem",
+    email: "Dallas@runo.biz",
+    body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid ab\nut fuga et molestiae est et"
+  },
+  {
+    postId: 2,
+    id: 9,
+    name: "repellat fugiat in",
+    email: "Mallory_Hyatt@davion.biz",
+    body: "repellat aliquid et rerum\nnumquam et omnis architecto excepturi optio deserunt\nexpedita eum et molestiae aut adipisci sint dolore pariatur"
+  },
+  {
+    postId: 2,
+    id: 10,
+    name: "et omnis dolorem",
+    email: "Dallas@runo.biz",
+    body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid ab\nut fuga et molestiae est et"
+  }
+];
